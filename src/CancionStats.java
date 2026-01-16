8febf39c77014bfd60d597e23a0b8c3db9d6bd52
dami <damianponte@hotmail.es>
2026-01-16 18:57:59 +0100

    constructor,metodo y getter hecho

--- src/CancionStats.java
+++ src/CancionStats.java
@@ -1,3 +1,32 @@
 public class CancionStats {
-    
+    //Crea una clase CancionStats con los atributos título y número de reproducciones.
+    //Añade un método reproducir() que incremente en uno el número de reproducciones.
+    //Desde la clase con main,
+    // simula varias reproducciones de una canción en un bucle con dicha función, y muestra el total usando el getter del objeto.
+
+        private String titulo;
+        private int reproducciones;
+
+    // Constructor
+    public CancionStats(String titulo) {
+        this.titulo = titulo;
+        this.reproducciones = 0;
+    }
+
+    // Método que incrementa el número de reproducciones
+    public void reproducir() {
+        reproducciones++;
+    }
+
+    //getters
+
+    public String getTitulo(){
+        return titulo;
+    }
+
+    public int getReproducciones(){
+        return reproducciones;
+    }
+
+
 }

c34df6d7a1af878cd078e1d15ff46a7e1fc49d21
dami <damianponte@hotmail.es>
2026-01-19 11:00:11 +0100

    empèzando ejercicio carrito,creando metodos

---
+++ src/Carrito.java
@@ -0,0 +1,5 @@
+public class Carrito {
+    //Crea una clase Carrito con un atributo total.
+    //Añade un método para añadir un producto al carrito y otro para aplicar un descuento porcentual.
+    //Desde la clase principal, añade varios productos y aplica un descuento al total.
+}

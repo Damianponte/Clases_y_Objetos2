edff5d06d143a7b0858fab2a6f44b8983a5c55b4
dami <damianponte@hotmail.es>
2026-01-19 11:49:57 +0100

    cambios realizados para llamar al metodo descuento

--- src/Carrito.java
+++ src/Carrito.java
@@ -3,16 +3,16 @@ public class Carrito {
     //Añade un método para añadir un producto al carrito y otro para aplicar un descuento porcentual.
     //Desde la clase principal, añade varios productos y aplica un descuento al total.
 
-    private double total;
+    private double getTotal;
 
 
-    public Carrito (int total){
-        this.total=total;
+    public Carrito() {
+        this.getTotal = 0;
     }
 
     public void añadirProducto(String producto,double precio){
         if (precio>=0){
-            total=total+precio;
+            getTotal=getTotal+precio;
             System.out.println("el producto: " + producto + " cuesta " + precio + " Euros");
 
         }else{
@@ -21,19 +21,21 @@ public class Carrito {
 
     }
 
-    public void descuento(double total){
-        if ( total>=30 && total<=60){
-            total=total - (total*0.20);
-        }else if ( total>60 && total<100){
-            total=total- (total*0.32);
-
-        }else{
-            total=total- (total*0.50);
+    public void descuento() {
+        if (getTotal >= 30 && getTotal <= 60) {
+            getTotal *= 0.80;
+        } else if (getTotal > 60 && getTotal < 100) {
+            getTotal *= 0.68;
+        } else if (getTotal >= 100) {
+            getTotal *= 0.50;
         }
 
     }
 
 
+    public double getTotal() {
+        return getTotal;
+    }
 
 }
 

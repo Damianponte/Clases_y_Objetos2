8813288eaeb6e2525a95daf46ac5928640bc8525
dami <damianponte@hotmail.es>
2026-01-19 11:52:14 +0100

    empezando ejercicio, inciando atributos y metodos.

--- src/MainCarrito.java
+++ src/MainCarrito.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class MainCarrito {
+    public static void main(String[] args) {
+
+        Carrito producto=new Carrito();
+        producto.añadirProducto("arroz",23);
+        producto.añadirProducto("ternera",15);
+        producto.añadirProducto("aceite",10);
+        System.out.println(" el total es:  " + producto.getTotal());
+
+
+        producto.descuento();
+        System.out.println(" el total con descuento es:  " + producto.getTotal());
+
+    }
+
 }
---
+++ src/Post.java
@@ -0,0 +1,5 @@
+public class Post {
+    //Crea una clase Post con los atributos título, número de likes y número de dislikes.
+    //Añade métodos para dar like, dar dislike y un método que devuelva la popularidad del post (likes menos dislikes).
+    //Desde el main, simula varias interacciones y muestra la popularidad.
+}

5f4129319aa6d4297f91176da228d99bf603d863
dami <damianponte@hotmail.es>
2026-01-19 10:58:48 +0100

    ejercicio main completado

--- src/MainJugador.java
+++ src/MainJugador.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
-
 public class MainJugador {
+    public static void main(String[] args) {
+        //Crea una clase Personaje con los atributos nombre, vida y ataque.
+        //Añade métodos para recibir daño, curarse y comprobar si el personaje sigue vivo. La vida no debe bajar de cero ni superar 100.
+        //Desde el main, simula un pequeño combate.
+        Personaje jugador=new Personaje ("Damian","lanzallamas",100,20);
+
+        System.out.println("el jugador llamado " + jugador.getNombre() + " tiene una vida de : " + jugador.getVida() + " de vida " );
+
+        System.out.println("han atacado a " + jugador.getNombre() + " con un  " + jugador.getNombreAtaque());
+
+
+        jugador.daño(75);
+        System.out.println("a  " + jugador.getNombre() + " le queda : " + jugador.getVida() + " de vida " );
+        System.out.println(jugador.getNombre() + " ha usado una pocion ");
+        jugador.curarse(50);
+        System.out.println(jugador.getNombre() + " se ha curado con una pocion y ahora le queda " + jugador.getVida() + " de vida " );
+
+
+    }
 }
